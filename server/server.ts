356e7b6d914883e8b6d7840fe2ef7bf1c62fa40e
Dmitry Kulak <dmitry.kulak11@gmail.com>
2022-04-01 18:40:30 +0300

    heroku setup

--- server/server.ts
+++ server/server.ts
@@ -5,8 +5,10 @@ import { burgers } from "./burgers";
 
 const app = express();
 const port = process.env.PORT || 3001;
+const staticFiles = express.static(path.join(__dirname, "../../"));
 
 app.use("/images", express.static(path.join(__dirname, "images")));
+app.use(staticFiles);
 app.use(express.json());
 
 app.get("/api/burgers/", (req, res) => {
@@ -17,5 +19,7 @@ app.post("/api/cart/", (req, res) => {
   console.log(req.body);
   res.send(req.body);
 });
+
+app.use("/*", staticFiles);
 
 app.listen(port, () => console.log(`Listening on port ${port}`));
--- src/api/api.ts
+++ src/api/api.ts
@@ -1,5 +1,7 @@
 import axios from "axios";
 
+const port = process.env.PORT || 5000;
+
 export const instance = axios.create({
-  baseURL: "http://localhost:3000/",
+  baseURL: `http://localhost:${port}/`,
 });
